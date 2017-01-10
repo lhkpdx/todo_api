20674a47b7c9ce279afadd138842afc5f8138f55
Laura Kibben <smartgirlpdx@gmail.com>
2017-01-10 15:21:44 -0800

    can create ojects from command line using curl

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,6 +7,10 @@ class ApiController < ApplicationController
      authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }
      end
 
+     def current_user
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]
+     end
+
      def permission_denied_error
        error(403, 'Permission Denied!')
      end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/create'
+
+  get 'lists/create'
+
   get 'users/index'
 
   namespace :api do
@@ -12,6 +16,14 @@ Rails.application.routes.draw do
   end
 
   namespace :api, defaults: { format: :json } do
-     resources :users
+    resources :users do
+      resources :lists
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
    end
 end
